507d67fc901cedddef32700cd092595a30221175
xiangminwang <wang@xiangmin.net>
2015-02-17 19:08:30 +0800

    release beta version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# oauth2-weibo
+# OAuth2-Weibo
 Weibo provider for league/oauth2-client
+
+## Installation
+```
+composer require xiangminwang/oauth2-weibo
+```
+
+## Usage
+```
+$provider = new XiangminWang\OAuth2\Client\Provider\Weibo([
+    'clientId' => '1104233555', // fill your "App Key"
+    'clientSecret' => '978N7tmeH7TMQsKy', // fill your "App Sercet"
+    'redirectUri' => 'http://example.com/oauth-endpoint',
+]);
+```
+
+## TODO
+
+- [x] Beta version
+- [ ] Unit tests
+- [ ] Full feature support
---
+++ src/Weibo.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace XiangminWang\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Entity\User;
+use League\OAuth2\Client\Token\AccessToken;
+
+class Weibo extends \League\OAuth2\Client\Provider\AbstractProvider
+{
+    public $responseType = 'string';
+    public $domain = 'https://api.weibo.com/oauth2';
+    public $apiDomain = 'https://api.weibo.com/2';
+    public $site = 'http://weibo.com/';
+
+    public function urlAuthorize()
+    {
+        return $this->domain.'/authorize';
+    }
+
+    public function urlAccessToken()
+    {
+        return $this->domain.'/access_token';
+    }
+
+    public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
+    {
+        return $this->apiDomain.'/users/show.json?access_token='.$token->accessToken.'&uid='.$token->uid;
+    }
+
+    public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
+    {
+        $user = new User;
+
+        $user->uid = $response->id;
+        $user->nickname = isset($response->name) ? $response->name : $response->domain;
+        $user->name = isset($response->screen_name) ? $response->screen_name : null;
+        $user->location = isset($response->location) ? $response->location : null;
+        $user->imageUrl = isset($response->avatar_large) ? $response->avatar_large : null;
+        $user->description = isset($response->description) ? $response->description : null;
+        $user->urls = [
+            'profile' => $site . (isset($response->profile_url) ? $response->profile_url : $response->id),
+            'site' => isset($response->url) && $response->url ? $response->url : null,
+        ];
+
+        return $user;
+    }
+
+    public function userUid($response, \League\OAuth2\Client\Token\AccessToken $token)
+    {
+        return $response->id;
+    }
+}
